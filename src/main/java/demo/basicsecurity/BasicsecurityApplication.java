9a8c91a12354a7394a34264d505fb573491883f2
DaeguIT-MinSuKim <net94@naver.com>
2021-06-13 16:20:40 +0900

    security initial complete

---
+++ src/main/java/demo/basicsecurity/BasicsecurityApplication.java
@@ -0,0 +1,13 @@
+package demo.basicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicsecurityApplication.class, args);
+	}
+
+}
